d82d443a46ceed6d23b499e534d14f1b9b555a8c
Rabin Awal <morbid_leathal@hotmail.com>
2019-06-10 00:25:16 -0400

    Create WebSecurityInitializer.java

---
+++ Configs/WebSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.rabin.secdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class WebSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
