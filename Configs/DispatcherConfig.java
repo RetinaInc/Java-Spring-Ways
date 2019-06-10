8ef2e10b1c72b358faad547cd0ccdb583e3d8bcf
Rabin Awal <morbid_leathal@hotmail.com>
2019-06-10 00:18:36 -0400

    Create DispatcherConfig.java

---
+++ Configs/DispatcherConfig.java
@@ -0,0 +1,25 @@
+package com.rabin.secdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {PersistanceConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {SpringConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
